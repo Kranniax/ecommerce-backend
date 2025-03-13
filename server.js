b70844328865357405211fe41208465c08807af8
Shane Wade <shanebramble96@hotmail.com>
2025-03-12 19:17:18 -0500

    initial commit

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const routes = require('./routes');
+import express from 'express';
+import routes from './routes';
 // import sequelize connection
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(json());
+app.use(urlencoded({ extended: true }));
 
 app.use(routes);
 
